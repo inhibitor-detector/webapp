1724fdcd0d401fb2a0106564f80681cc067f591b
mickyconca <mickyconca@hotmail.com>
2024-06-17 17:05:20 -0300

    feat: alert when new inhibition is detected

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import DetectorTable from './components/DetectorTable';
 import SignalTable from './components/SignalTable';
 import SignIn from './components/Signin';
 import { Route, Routes } from 'react-router-dom';
-import AlertContainer from './components/AlertContainer';
 import PrivateRoutes from './components/PrivateRoutes'
 import HeartbeatTable from './components/HeartbeatTable';
-import { startLoginInterval, stopLoginInterval } from './components/AuthService';
-import { useAuth } from './components/AuthContext';
+import InhibitionDetected from './components/InhibitionDetected';
 
 const App = () => {
-  const [open, setOpen] = useState(true);
-  const { saveToken } = useAuth();
-
-  useEffect(() => {
-    const intervalId = startLoginInterval(saveToken);
-    return () => {
-      stopLoginInterval(intervalId);
-    };
-  }, [saveToken]);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   return (
     <div>
-      <AlertContainer open={open} onClose={handleClose}/>
+      <InhibitionDetected />
       <Routes>
       <Route path='/' element={<SignIn/>}/>
         <Route element={<PrivateRoutes />}>
--- front-end/src/components/AlertContainer.js
+++ front-end/src/components/AlertContainer.js
@@ -7,7 +7,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import { Typography } from '@mui/material';
 
 
-const Notification = ({ open, onClose }) => {
+const Notification = ({ open, onClose, detectorId }) => {
   return (
     <div>
       {open && (
@@ -58,10 +58,8 @@ const Notification = ({ open, onClose }) => {
               fontSize: 24
             }}
           >
-            Inhibidor detectado
+            Inhibidor detectado por detector: {detectorId}
           </Typography>
-          {/* <h3 style={{ color: 'white', textAlign: 'center' }}>Detector: {selectedDetector.id}</h3>
-            <h3 style={{ color: 'white', textAlign: 'center' }}>Descripción: {selectedDetector.name}</h3>  */}
         </div>
       </Modal>
     </div>
