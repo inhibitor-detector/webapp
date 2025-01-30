4088ccc3d975bce01bcc6d3775146b93a6f98658
Urix <43704209+uri-99@users.noreply.github.com>
2025-01-30 20:52:42 -0300

    fix: add import LocalDateTime

--- api/models/src/test/java/ar/edu/itba/tesis/models/DetectorTest.java
+++ api/models/src/test/java/ar/edu/itba/tesis/models/DetectorTest.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDateTime;
+
 @ExtendWith(MockitoExtension.class)
 class DetectorTest {
 
--- api/webapp/src/main/java/ar/edu/itba/tesis/webapp/dtos/DetectorDto.java
+++ api/webapp/src/main/java/ar/edu/itba/tesis/webapp/dtos/DetectorDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 import java.util.List;
 import java.util.Set;
