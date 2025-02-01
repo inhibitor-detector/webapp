88c5fb4b39648ef001951958ca987d3303e8c874
Urix <43704209+uri-99@users.noreply.github.com>
2025-02-01 18:17:24 -0300

    feat(Wip): send mails on inhibition detected

---
+++ api/services/src/main/java/ar/edu/itba/tesis/services/EmailServiceImpl.java
@@ -0,0 +1,23 @@
+package ar.edu.itba.tesis.services;
+
+import ar.edu.itba.tesis.interfaces.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Override
+    public void sendInhibitionDetectedEmail(String to, String detectorName, String timestamp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Inhibition Detected - " + detectorName);
+        message.setText("An inhibition signal was detected by detector '" + detectorName + "' at " + timestamp);
+        emailSender.send(message);
+    }
+}
